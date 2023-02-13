1cfb9ddd8257d6f932a3136d7aa5bf9e6ba6b174
Ali Ahmad <ali.ahmed@tintash.com>
2023-02-13 17:42:56 +0500

    feat: jwt
    what: added jwt authentication
    
    why: so that we can protect routes

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,13 @@
-from django.urls import path
-from .views import LoginAPIView, RegisterView
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.views import TokenRefreshView
 
+from django.urls import path
+
+from .views import RegisterView
 
 urlpatterns = [
-    path("login", LoginAPIView.as_view(), name="auth_login"),
+    path("login", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    # path("login", LoginAPIView.as_view(), name="auth_login"),
     path("register", RegisterView.as_view(), name="auth_register"),
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,13 +1,16 @@
-from rest_framework import viewsets
-from .serializers import NoteSerializer
-from .models import Note
-from rest_framework.permissions import IsAuthenticated
 from rest_framework import filters
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+
+from .models import Note
+from .serializers import NoteSerializer
 
 
 class NoteViewSet(viewsets.ModelViewSet):
     queryset = Note.objects.all()
-    permission_classes = (IsAuthenticated,)
+    permission_classes = [
+        IsAuthenticated,
+    ]
     serializer_class = NoteSerializer
     lookup_field = "pk"
     filter_backends = [filters.SearchFilter]
